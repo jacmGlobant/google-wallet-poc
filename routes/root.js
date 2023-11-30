43ad0d083fabe94054aab68cc745b0774a8f60f0
Juan Canepa <juan.canepa@globant.com>
2023-11-30 15:47:38 -0300

    fix: added helth url

--- routes/root.js
+++ routes/root.js
@@ -1,10 +1,12 @@
 'use strict'
 
 const loyaltycard = require('./loyaltycard');
+const health = require('./health');
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     return { root: true }
   })
   fastify.register(loyaltycard);
+  fastify.register(health);
 }
